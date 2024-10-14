feee38c55d247bdc229a68f89989e0429b1cf156
ia.safonov1 <ia.safonov1@tensor.ru>
2024-10-14 11:36:09 +0300

    '2'

--- pages/dialog_page.py
+++ pages/dialog_page.py
@@ -2,30 +2,41 @@ from atf.ui import *
 from controls import *
 
 
-class Dialog(Region):
+class MainPage(Region):
     diaologs = ControlsListView(By.CSS_SELECTOR, '.msg-dialogs-detail__list .controls-ListViewV__itemsContainer', 'Диалоги')
-    chats = ControlsListView(By.CSS_SELECTOR, '.msg-CorrespondenceDetail .controls-ListViewV__itemsContainer', 'Чаты')
+    tabs = ControlsTabsButtons()
+    folders = ControlsTreeGridView(By.CSS_SELECTOR, '.controls-MasterDetail_master-template .controls-Grid', 'Папки')
+    tags_list = ControlsListView(By.CSS_SELECTOR, '.tags-list', 'Теги')
+
+    count_folder = folders.row(contains_text='папка для перемещения').should_be(Displayed).element('[data-qa="msg-folders-counter_total"]')
+    date_message = diaologs.item(contains_text='Сообщение').should_be(Displayed).element('.msg-entity-date')
+    count_tag = tags_list.item(contains_text='Тест').should_be(Displayed).element('[data-qa="tag-count-entities"]')
+    marker_folders = folders.row(contains_text='папка для перемещения').element('[data-qa="marker"]')
+    marker_tabs = tabs.item(contains_text='Чаты').element('.controls-Tabs__marker')
 
     def check_load(self):
         self.diaologs.check_load()
 
 
-class Folders(Region):
-    folders = ControlsTreeGridView(By.CSS_SELECTOR, '.controls-MasterDetail_master-template .controls-Grid', 'Папки')
-    tags_list = ControlsListView(By.CSS_SELECTOR, '.tags-list', 'Теги')
+class Chats(Region):
+    chats = ControlsListView(By.CSS_SELECTOR, '.msg-CorrespondenceDetail .controls-ListViewV__itemsContainer', 'Чаты')
 
+    date_message = chats.item(contains_text='Сообщение').element('.msg-entity-date')
 
     def check_load(self):
-        self.folders.check_load()
+        self.chats.check_load()
 
 
-class Panel(Region):
+class PanelMoveDialog(Region):
     move_dialog = ControlsTreeGridView(By.CSS_SELECTOR, '.controls-Popup .controls-Grid', "Список для перемещения")
-    control_panel = ControlsListView(By.CSS_SELECTOR, '.controls-Popup .controls-ListViewV', "Список для тегов")
 
     def check_load(self):
         self.move_dialog.check_load()
 
 
-class Tabs(Region):
-    tabs = ControlsTabsButtons()
+class ControlPanel(Region):
+    panel = ControlsListView(By.CSS_SELECTOR, '.controls-Popup .controls-ListViewV', "Список для тегов")
+
+    def check_load(self):
+        self.panel.check_load()
+
--- test.py
+++ test.py
@@ -1,57 +1,74 @@
 from atf import *
 from atf.ui import *
 from pages.auth_page import AuthPage
-from pages.dialog_page import Dialog, Panel, Folders, Tabs
+from pages.dialog_page import MainPage, Chats, PanelMoveDialog, ControlPanel
 
 
 class Test(TestCaseUI):
+    message = 'Сообщение'
 
     @classmethod
     def setUpClass(cls):
         AuthPage(cls.driver).auth()
-        cls.dialog = Dialog(cls.driver)
-        cls.panel = Panel(cls.driver)
-        cls.folder = Folders(cls.driver)
-        cls.tab = Tabs(cls.driver)
+        cls.main = MainPage(cls.driver)
+        cls.chats = Chats(cls.driver)
+        cls.move_panel = PanelMoveDialog(cls.driver)
+        cls.control_panel = ControlPanel(cls.driver)
 
     def setUp(self):
-        self.dialog.check_load()
+        self.main.check_load()
+
+    def tearDown(self):
+        self.browser.close_windows_and_alert()
 
     def test_01_move_message(self):
-        text_message = self.dialog.diaologs.item(contains_text='Розница1 Автотестер Продавец').text
-        folder = self.folder.folders.row(contains_text='папка для перемещения')
+        """Перемещение сообщения в папку"""
 
-        self.dialog.diaologs.item(contains_text='Розница1 Автотестер Продавец').select_menu_actions("Переместить")
-        self.panel.check_load()
-        self.panel.move_dialog.row(contains_text='папка для перемещения').click()
+        folder_to_move = 'папка для перемещения'
+        text_message = self.main.diaologs.item(contains_text=self.message).should_be(Displayed).text
 
+        self.main.diaologs.item(contains_text=text_message).select_menu_actions("Переместить")
+        self.move_panel.check_load()
+        self.move_panel.move_dialog.row(contains_text=folder_to_move).click()
+        self.move_panel.move_dialog.should_not_be(Displayed)
+
+        folder = self.main.folders.row(contains_text=folder_to_move)
         folder.click()
-        self.dialog.diaologs.item(item_number=1).should_be(ContainsText(text_message))
-        folder.element('[data-qa="msg-folders-counter_total"]').should_be(ExactText('1'))
+        self.main.marker_folders.should_be(Displayed)
+        self.main.diaologs.item(with_text=text_message).should_be(Displayed)
+        self.main.count_folder.should_be(ExactText('1'))
 
-        self.dialog.diaologs.item(contains_text='Розница1 Автотестер Продавец').select_menu_actions("Переместить")
-        self.panel.check_load()
-        self.panel.move_dialog.row(contains_text="Все сообщения").element('div').click()
-        self.dialog.diaologs.should_be(CountElements(0))
-        self.folder.folders.row(contains_text='Все сообщения').click()
+        self.main.diaologs.item(contains_text=text_message).select_menu_actions("Переместить")
+        self.move_panel.check_load()
+        self.move_panel.move_dialog.row(contains_text="Все сообщения").element('div').click()
+        self.move_panel.move_dialog.should_not_be(Displayed)
+        self.main.diaologs.should_be(CountElements(0))
 
     def test_02_check_date(self):
-        date_message = self.dialog.diaologs.item(contains_text='Розница1 Автотестер Продавец').element('.msg-entity-date').text
-        self.tab.tabs.select("Чаты")
-        self.dialog.chats.item(contains_text='Розница1 Автотестер Продавец').element('.msg-entity-date').should_be(ExactText(date_message))
-        self.tab.tabs.select(contains_text="Диалоги")
+        """Проверка даты сообщения"""
+
+        date_message = self.main.date_message.text
+        self.main.tabs.select("Чаты")
+        self.main.marker_tabs.should_be(Displayed)
+        self.chats.chats.item(contains_text=self.message).should_be(Displayed)
+        self.chats.date_message.should_be(ExactText(date_message))
 
     def test_03_select_tag(self):
-        message = self.dialog.diaologs.item(contains_text='Розница1 Автотестер Продавец')
+        """Пометить сообщение тегом"""
+        test = 'Тест'
+        message = self.main.diaologs.item(contains_text=self.message)
         message.select_menu_actions("Пометить")
-        self.panel.control_panel.check_load()
-        self.panel.control_panel.item(contains_text='Тест').click()
+        self.control_panel.panel.check_load()
+        self.control_panel.panel.item(contains_text=test).click()
+        self.control_panel.panel.should_not_be(Displayed)
 
-        self.folder.tags_list.item(contains_text='Тест').element('[data-qa="tag-count-entities"]').should_be(ExactText('1'))
-        message.should_be(ContainsText('Тест'))
+        self.main.count_tag.should_be(ExactText('1'))
+        message.should_be(ContainsText(test))
 
         message.select_menu_actions("Пометить")
-        self.panel.control_panel.check_load()
-        self.panel.control_panel.item(contains_text='Тест').click()
+        self.control_panel.panel.check_load()
+        self.control_panel.panel.item(contains_text=test).click()
+        self.control_panel.panel.should_not_be(Displayed)
+        message.should_not_be(ContainsText(test))
 
 
